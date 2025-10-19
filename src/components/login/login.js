f9773b11a84139078a3d0501c0ec7d5350343f0a
TEC-ALVARO <l23211931@tectijuana.edu.mx>
2025-10-19 14:12:32 -0700

    net app first commit

---
+++ src/components/login/login.js
@@ -0,0 +1,41 @@
+export function Login()
+{
+return (
+    <div>
+        <div>
+            
+        </div>
+        <div>
+            <h3>
+                Iniciar Sesión
+            </h3>
+        </div>
+        <div>
+            <input
+            type="email"
+            ></input>
+        </div>
+        <div>
+            <input
+            type="password"
+            ></input>
+        </div>
+        <div>
+            <button>
+                Iniciar Sesión
+            </button>
+            
+        </div>
+        <div>
+            <a href="#">
+                ¿Olvidaste tu contraseña?
+            </a>
+        </div>
+        <div>
+            <button>
+                Crear cuenta nueva
+            </button>
+        </div>
+    </div>
+)
+}
